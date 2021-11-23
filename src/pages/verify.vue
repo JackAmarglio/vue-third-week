55301bf4e0277a10faaffa2dc25f329b82798070
LeeMing-creator <90887612+LeeMing-creator@users.noreply.github.com>
2021-11-23 16:56:21 +0800

    Update verify.vue

--- src/pages/verify.vue
+++ src/pages/verify.vue
@@ -9,6 +9,7 @@
 import axios from "axios";
 export default {
     mounted() {
+        
         axios({
             url: "https://byself-laravel.herokuapp.com/api/verify",
             data: {token:this.$route.params.token},
